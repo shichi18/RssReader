0986d443cdd56387a9adc5ecdec49d7bc68c7c81
kumaneko <n.kumaneko18@gmail.com>
2019-04-15 22:21:44 +0900

    [add] Add http connection setting.

---
+++ app/src/main/java/com/github/shichi18/rssreader/Http.kt
@@ -0,0 +1,30 @@
+package com.github.shichi18.rssreader
+
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+fun hettpGet(url: String): InputStream? {
+    //通信接続用のオブジェクト生成
+    val con = URL(url).openConnection() as HttpURLConnection
+
+    //接続の設定
+    con.apply {
+        requestMethod = "GET"
+        connectTimeout = 3000
+        readTimeout = 5000
+        instanceFollowRedirects = true
+    }
+
+    //接続
+    con.connect()
+
+    //ステータスコード
+    if (con.responseCode in 200..299){
+        //成功したらレスポンスの入力ストリームをBufferedInputStreamとして返す
+        return BufferedInputStream(con.inputStream)
+    }
+    //失敗
+    return null
+}
